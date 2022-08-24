a6ba4e89494aa2014bddd57e9f7dc4b66a15605d
harshita <harshita1225@gmail.com>
2022-08-24 15:29:06 +0200

    task completed

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 class Employee {
-	constructor (firstname, lastname) {
-		this.firstname = firstname
-		this.lastname = lastname
-		// Complete the code!
-	}
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    // Complete the code!
+  }
+  fullname() {
+    console.log(this.firstname + this.lastname);
+  }
+
+  email() {
+    console.log(`${this.firstname}.${this.lastname}@company.com`);
+  }
 }
+
+emp1 = new Employee("John", "Smith");
+emp1.fullname(); //"John Smith"
+
+emp2 = new Employee("Mary", "Sue");
+emp2.email(); //"mary.sue@company.com"
+
+emp3 = new Employee("Antony", "Walker");
+console.log(emp3.firstname);
+// "Antony"
